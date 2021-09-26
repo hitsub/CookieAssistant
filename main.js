d1f6f849901571e64a85c97ce9219e8f5bbf4acb
Hitsub <hitsumabushis@gmail.com>
2021-09-26 21:20:21 +0900

    0.6.0にバージョンアップ

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ if(typeof CCSE == 'undefined')
 }
 
 CookieAssistant.name = 'Cookie Assistant';
-CookieAssistant.version = '0.5.0';
+CookieAssistant.version = '0.6.0';
 CookieAssistant.GameVersion = '2.042';
 
 
