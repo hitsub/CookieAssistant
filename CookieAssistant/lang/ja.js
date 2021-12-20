b0655fd5a1756d9c68b35b4df71727d7a863d8a1
t-nishiyama <t-nishiyama@colopl.co.jp>
2021-12-20 15:24:08 +0900

    インデント修正

--- CookieAssistant/lang/ja.js
+++ CookieAssistant/lang/ja.js
@@ -1,7 +1,7 @@
 ModLanguage('JA', {
-    // アプデ関係
+	// アプデ関係
 	"There will be an update." : "アップデートがあります。",
-    "Download Here." : "ここからダウンロードしてください。",
-    "It's been updated." : "アップデートされました。",
-    "You can check the patch notes from here." : "ここから更新内容が確認できます。",
+	"Download Here." : "ここからダウンロードしてください。",
+	"It's been updated." : "アップデートされました。",
+	"You can check the patch notes from here." : "ここから更新内容が確認できます。",
 });
