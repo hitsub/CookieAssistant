044bcbf226dabe7b375af3f65dd43b2a2421164e
t-nishiyama <t-nishiyama@colopl.co.jp>
2021-12-20 15:21:37 +0900

    アプデ後の初回起動時、リリースノートの通知を行う

--- CookieAssistant/lang/ja.js
+++ CookieAssistant/lang/ja.js
@@ -1,5 +1,7 @@
 ModLanguage('JA', {
     // アプデ関係
 	"There will be an update." : "アップデートがあります。",
-    "Download Here." : "ここからダウンロードしてください。"
+    "Download Here." : "ここからダウンロードしてください。",
+    "It's been updated." : "アップデートされました。",
+    "You can check the patch notes from here." : "ここから更新内容が確認できます。",
 });
